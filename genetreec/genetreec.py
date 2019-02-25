57a0a2ea32e9fb5d9b41d963a2f98e33c1524a68
Miguel <torresma.1996@gmail.com>
2019-02-25 19:30:26 +0100

    start genetreec

---
+++ genetreec/genetreec.py
@@ -0,0 +1,26 @@
+class Genetreec:
+	root  #first Node
+
+	def __init__(self):
+		
+
+	def train(self, data):
+		
+
+	def test(self, data):
+		
+
+
+class Node:
+	feature  #column to split the data
+	pivot    #value to split the data
+
+	def __init__(self, feature, pivot):
+		
+
+class Leaf:
+	tag
+
+	def __init__(self):
+
+
