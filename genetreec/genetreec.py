2e05f5ffc89b19bd56abc7f2f6baac10b6b55905
Miguel <torresma.1996@gmail.com>
2019-02-25 21:11:59 +0100

    new

--- genetreec/genetreec.py
+++ genetreec/genetreec.py
@@ -1,9 +1,9 @@
 class Genetreec:
 	root  #first Node
 
-	def __init__(self):
+	def __init__(self,data):
+		self.root = Leaf(data)
 		
-
 	def train(self, data):
 		
 
@@ -12,15 +12,20 @@ class Genetreec:
 
 
 class Node:
-	feature  #column to split the data
-	pivot    #value to split the data
+	feature = None  #column to split the data
+	pivot = None    #value to split the data
+
+	right = None   #node or leaf positive
+	left = None    #node or leaf negative
 
 	def __init__(self, feature, pivot):
-		
+		self.feature = feature
+		self.pivot = pivot
 
 class Leaf:
-	tag
+	tag = None   #the final tag the data on the leaf will be classificated as (Used just when trained)
+	data = None  #the partition of data which verifies the branch's nodes restrictions
 
-	def __init__(self):
+	def __init__(self, data):
 
 
