0499bde1720555a2c740d3308014e237eccc80e7
Miguel <torresma.1996@gmail.com>
2019-03-18 19:06:09 +0100

    first tagger

--- genetreec/tagger.py
+++ genetreec/tagger.py
@@ -1,50 +1,86 @@
 import pandas as pd
 import numpy as np
+import talib
+import math
 
 def acumtag(data):
-	data['dif'] = ''
+	print(data.shape)
+	data['dif'] = talib.MOM(data['Close'],1)
+	data['dif'][0] = -0.01
+	data_tag = [0] * data.shape[0]
+
 	negative_acum = 0
 	positive_acum = 0
 	max_negative_acum = 0
 	max_positive_acum = 0
 
 	positive = True
-	actual = data['Close'][0]
-	size = data.shape[0]
 	
-
-	for i in range(1, size) :
+	# Taking the max positive and negative diference consecutive 
+	for dif in data['dif']:
 		if positive:
-			data['dif'][i] = data['Close'][i] - data['Close'][i-1]
-			actual = actual + data['dif'][i]			
-			if data['Close'][i] - data['Close'][i-1] > 0: 
-				max_positive_acum = max([actual, max_positive_acum])
+			if dif > 0:
+				positive_acum += dif
 			else:
+				max_positive_acum = max(positive_acum, max_positive_acum)
+				negative_acum = dif
 				positive = False
-				actual = data['Close'][i] - data['Close'][i-1]
-				data['dif'][i] = actual
-				max_negative_acum = min([actual, max_negative_acum])
-
+					
 		else:
-			data['dif'][i] = data['Close'][i] - data['Close'][i-1]
-			actual = actual + data['dif'][i]
-			if data['Close'][i] - data['Close'][i-1] < 0: 
-				max_negative_acum = min([actual, max_negative_acum])
+			if dif < 0:
+				negative_acum += dif
 			else:
+				max_negative_acum = min(negative_acum, max_negative_acum)
+				positive_acum = dif
 				positive = True
-				actual = data['Close'][i] - data['Close'][i-1]
-				data['dif'][i] = actual
-				max_positive_acum = max([actual, max_positive_acum])
-	
-	print(max_negative_acum)
-	print(max_positive_acum)
+
+
+	positive = False
+	negative_acum = 0
+	positive_acum = 0
+	ichange = 0
+	max_negative_acum = (-max_negative_acum)/6
+	max_positive_acum = (max_positive_acum)	/6
+
+
+	for dif,i in zip(data['dif'],list(range(0,data.shape[0]))): 
+		if positive:
+			if dif < 0:
+				negative_acum += dif
+				if negative_acum < max_negative_acum:
+					data_tag[ichange] = 1 #sell
+					positive = False
+					positive_acum = 0
+					ichange = i+1
+			else:
+				negative_acum += dif
+				if negative_acum > 0 :
+					negative_acum = 0
+				if negative_acum == 0:
+					ichange = i
+		else:
+			if dif > 0:
+				positive_acum += dif
+				if positive_acum > max_positive_acum:
+					print(i)
+					data_tag[ichange] = -1 #buy
+					positive = True
+					negative_acum = 0
+					ichange = i+1
+			else:
+				positive_acum += dif
+				if positive_acum < 0 :
+					positive_acum = 0
+				if positive_acum == 0:
+					ichange = i
+					
+	data['tag'] = data_tag
+	print('max_neg_acum:' + str(max_negative_acum) + ' max_pos_acum:' + str(max_positive_acum))
+	np.set_printoptions(threshold=0)
 	print(data)
 
+
 df = pd.read_csv('../Data/SAN.csv')
-acumtag(df[['Close']])
-
-vector = [1,2,3,4,5]
-
-vector2 = [f(x) for x in vector]
+acumtag(df)
 
 
