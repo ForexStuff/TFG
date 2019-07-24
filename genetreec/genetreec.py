08ec5b1edba096b3544a7c32cde6c317a64f7cbb
Miguel <torresma.1996@gmail.com>
2019-07-24 12:27:31 +0200

    Reorganize core code

--- genetreec/genetreec.py
+++ genetreec/genetreec.py
@@ -72,210 +72,208 @@ class EndStats(bt.Analyzer):
 
 
 
+class Simulate:
+	data = None
+	nextpopulation = None
+	numbertree = 20
+	numberiter = 10
+	start_date_train = "2013-08-01"
+	end_date_train   = "2014-02-26"
+	start_date_test  = "2014-02-26"
+	end_date_test    = "2014-08-01"
 
 
-# Dados dos árboles, intercambia 'aleatoriamente' dos de sus ramas.
-def Crossover(atree, btree):
-	aside, abranch = atree.selectRandomBranch()
-	bside, bbranch = btree.selectRandomBranch()
+	# Dados dos árboles, intercambia 'aleatoriamente' dos de sus ramas.
+	def Crossover(self, atree, btree):
+		aside, abranch = atree.selectRandomBranch()
+		bside, bbranch = btree.selectRandomBranch()
 
-	auxbranch = None
-	if aside == "left":
-		auxbranch = abranch.left
-		if bside == "left":
-			abranch.left = bbranch.left
-			bbranch.left = auxbranch
-		elif bside == "right":
-			abranch.left = bbranch.right
-			bbranch.right = auxbranch
+		auxbranch = None
+		if aside == "left":
+			auxbranch = abranch.left
+			if bside == "left":
+				abranch.left = bbranch.left
+				bbranch.left = auxbranch
+			elif bside == "right":
+				abranch.left = bbranch.right
+				bbranch.right = auxbranch
+			else:
+				abranch.left = bbranch.root
+				bbranch.root = auxbranch
+		elif aside == "right":
+			auxbranch = abranch.right
+			if bside == "left":
+				abranch.right = bbranch.left
+				bbranch.left = auxbranch
+			elif bside == "right":
+				abranch.right = bbranch.right
+				bbranch.right = auxbranch
+			else:
+				abranch.right = bbranch.root
+				bbranch.root = auxbranch
 		else:
-			abranch.left = bbranch.root
-			bbranch.root = auxbranch
-	elif aside == "right":
-		auxbranch = abranch.right
-		if bside == "left":
-			abranch.right = bbranch.left
-			bbranch.left = auxbranch
-		elif bside == "right":
-			abranch.right = bbranch.right
-			bbranch.right = auxbranch
+			auxbranch = abranch.root
+			if bside == "left":
+				abranch.root = bbranch.left
+				bbranch.left = auxbranch
+			elif bside == "right":
+				abranch.root = bbranch.right
+				bbranch.right = auxbranch
+			else:
+				abranch.root = bbranch.root
+				bbranch.root = auxbranch
+
+		return atree, btree
+
+
+	# Dada una población y sus puntuaciones, devuelve la población del
+	# algoritmo con sus probabilidades reproductivas.
+	def Reproductivity(self, score):
+		pop_score = pd.DataFrame()
+		pop_score['tree'] = [tree for tree in self.population]
+		pop_score['score'] = score
+		pop_score['score'] += [tree.sellcount * 10 for tree in self.population]
+		pop_score = pop_score.sort_values(by=['score'], ascending=False)
+
+		# Escalado Min-max para sacar probabilidades (score al intervalo [0,1])
+		pop_score['score'] -= pop_score['score'].min()
+		aux = pop_score['score'].sum()
+		if aux == 0:
+			aux = 1/pop_score.shape[0]
+			pop_score['score'] = [num*aux for num in range(1,pop_score.shape[0]+1)]
 		else:
-			abranch.right = bbranch.root
-			bbranch.root = auxbranch
-	else:
-		auxbranch = abranch.root
-		if bside == "left":
-			abranch.root = bbranch.left
-			bbranch.left = auxbranch
-		elif bside == "right":
-			abranch.root = bbranch.right
-			bbranch.right = auxbranch
-		else:
-			abranch.root = bbranch.root
-			bbranch.root = auxbranch
+			aux = 1/aux
+			pop_score['score'] *= aux
+			pop_score['score'] = pop_score['score'].cumsum()
 
-	return atree, btree
+		print(pop_score)
+		return pop_score
 
 
 
+	# Dada la población de una iteración, devuelve la de la siguiente.
+	def NextPopulation(self, score):
+		self.nextpopulation = []
+		popu_reprod = self.Reproductivity(score)
+
+		# Los 2 mejores los guardo siempre
+		buy, sell = popu_reprod.iloc[0]['tree'].getBuySell()
+		print('buy=' + str(buy) + ', sell=' + str(sell))
+		popu_reprod.iloc[2]['tree'].mutate()
+		popu_reprod.iloc[3]['tree'].mutate()
+		self.nextpopulation.append(popu_reprod.iloc[0]['tree'])
+		self.nextpopulation.append(popu_reprod.iloc[1]['tree'])
+		self.nextpopulation.append(popu_reprod.iloc[2]['tree'])
+		self.nextpopulation.append(popu_reprod.iloc[3]['tree'])
+
+		while self.numbertree > len(self.nextpopulation):
+			auni = np.random.uniform(0,1,2)
+			atree = (popu_reprod['tree'][popu_reprod['score'] >= auni[0]]).iloc[0]
+			btree = (popu_reprod['tree'][popu_reprod['score'] >= auni[1]]).iloc[0]
+			atree, btree = self.Crossover(deepcopy(atree), deepcopy(btree))
+			aux = atree.getNumNodes()
+			if aux < 35 and aux > 5:
+				atree.mutate() #Mutación
+				self.nextpopulation.append(atree)
+			aux = btree.getNumNodes()
+			if aux < 35 and aux > 5:
+				btree.mutate() #Mutación
+				self.nextpopulation.append(btree)
+
+		i=0
+		for tree in self.nextpopulation:
+			tree.ind = i
+			i += 1
+		self.population = self.nextpopulation
+		return None
 
 
-# Dada una población y sus puntuaciones, devuelve la población del
-# algoritmo con sus probabilidades reproductivas.
-def Reproductivity(population, score):
-	pop_score = pd.DataFrame()
-	pop_score['tree'] = [tree for tree in population]
-	pop_score['score'] = score
-	pop_score['score'] += [tree.sellcount * 10 for tree in population]
-	pop_score = pop_score.sort_values(by=['score'], ascending=False)
+	def prepare(self):
+		tagger.acumtag(self.start_date_train, self.end_date_train)   # Solo se ejecuta la primera vez, el etiquetado es lento
+							# y mejor hacerlo solo una vez
+		self.data = pd.read_csv('tagged_data/SAN.csv')
+		self.population = []
 
-	# Escalado Min-max para sacar probabilidades (score al intervalo [0,1])
-	pop_score['score'] -= pop_score['score'].min()
-	aux = pop_score['score'].sum()
-	if aux == 0:
-		aux = 1/pop_score.shape[0]
-		pop_score['score'] = [num*aux for num in range(1,pop_score.shape[0]+1)]
-	else:
-		aux = 1/aux
-		pop_score['score'] *= aux
-		pop_score['score'] = pop_score['score'].cumsum()
+		for i in range(self.numbertree):   # Calentamiento de la población 1
+			tree = gentree(i)
+			tree.warm()
+			self.population.append(tree)
+			print('Tree ' + str(i) + ' warmed.')
 
-	print(pop_score)
-	return pop_score
-
-
-
-
-
-
-# Dada la población de una iteración, devuelve la de la siguiente.
-def NextPopulation(population, score):
-	saveLen = int(len(population)/2-1)
-	nextpopulation = []
-	popu_reprod = Reproductivity(population,score)
-
-	# Los 2 mejores los guardo siempre
-	buy, sell = popu_reprod.iloc[0]['tree'].getBuySell()
-	print('buy=' + str(buy) + ', sell=' + str(sell))
-	popu_reprod.iloc[2]['tree'].mutate()
-	popu_reprod.iloc[3]['tree'].mutate()
-	nextpopulation.append(popu_reprod.iloc[0]['tree'])
-	nextpopulation.append(popu_reprod.iloc[1]['tree'])
-	nextpopulation.append(popu_reprod.iloc[2]['tree'])
-	nextpopulation.append(popu_reprod.iloc[3]['tree'])
-
-	while numbertree > len(nextpopulation):
-		auni = np.random.uniform(0,1,2)
-		atree = (popu_reprod['tree'][popu_reprod['score'] >= auni[0]]).iloc[0]
-		btree = (popu_reprod['tree'][popu_reprod['score'] >= auni[1]]).iloc[0]
-		atree, btree = Crossover(deepcopy(atree), deepcopy(btree))
-		aux = atree.getNumNodes()
-		if aux < 35 and aux > 5:
-			atree.mutate() #Mutación
-			nextpopulation.append(atree)
-		aux = btree.getNumNodes()
-		if aux < 35 and aux > 5:
-			btree.mutate() #Mutación
-			nextpopulation.append(btree)
-
-	i=0
-	for tree in nextpopulation:
-		tree.ind = i
-		i += 1
-	return nextpopulation
+	def execute(self):
+		simudatos = pdr.get_data_yahoo("SAN", start=self.start_date_train, end=self.end_date_train)
+		# df = yf.download("SAN", start="2017-01-01", end="2017-04-30")  # Otra forma de coger los datos
+		df_cerebro = bt.feeds.PandasData(dataname = simudatos)
+		indicator.setData(simudatos)
 
 
 
+		cerebro = bt.Cerebro(maxcpus=1)
+		cerebro.optstrategy(TreeStrategy,tree=list(self.population))   # Seleccionar estrategia
+		cerebro.addanalyzer(EndStats)						      # Seleccionar analizador
+		cerebro.adddata(df_cerebro)										  # Seleccionar datos
+		cerebro.broker.set_coc(True)
+		cerebro.broker.setcash(10000.0)	# Seleccionar dinero
+		cerebro.broker.setcommission(commission=0.01)
+
+		for i in range(self.numberiter):
+			ts = time.time()
+			ret = cerebro.run()   # EJECUTAR BACKTESTING
+
+			scores = pd.DataFrame({r[0].params.tree.ind: r[0].analyzers.endstats.get_analysis() for r in ret}
+			                      ).T.loc[:, ['end', 'growth', 'return']]
+
+			print(scores)
+			self.NextPopulation(scores['end'])
+			# Aquí debería ir la mutación
+			# Por motivos de eficiencia se hace dentro de NextPopulation
+
+			te = time.time()
+			print("El tiempo de simulación es: ",(te - ts))
+
+			indicator.setData(simudatos)
+			cerebro = bt.Cerebro(maxcpus=None)
+			cerebro.optstrategy(TreeStrategy,tree=list(self.population))   # Seleccionar estrategia
+			cerebro.addanalyzer(EndStats)						      # Seleccionar analizador
+			cerebro.adddata(df_cerebro)										  # Seleccionar datos
+			cerebro.broker.set_coc(True)
+			cerebro.broker.setcash(10000.0)	# Seleccionar dinero
+			cerebro.broker.setcommission(commission=0.01)
+
+			tot = 0
+			for tree in self.population:
+				tot+=tree.getNumNodes()
+			print(tot/len(self.population))
 
 
-numbertree = 20
-numberiter = 20
-start_date_train = "2013-08-01"
-end_date_train   = "2014-02-26"
-start_date_test  = "2014-02-26"
-end_date_test    = "2014-08-01"
-tagger.acumtag(start_date_train, end_date_train)   # Solo se ejecuta la primera vez, el etiquetado es lento
-					# y mejor hacerlo solo una vez
-data = pd.read_csv('tagged_data/SAN.csv')
-population = []
+		ret = cerebro.run()
+		scores = pd.DataFrame({r[0].params.tree.ind: r[0].analyzers.endstats.get_analysis() for r in ret}
+		                      ).T.loc[:, ['end', 'growth', 'return']]
 
-for i in range(numbertree):   # Calentamiento de la población 1
-	tree = gentree(i)
-	tree.warm()
-	population.append(tree)
-	print('Tree ' + str(i) + ' warmed.')
+		pop_score = pd.DataFrame()
+		pop_score['tree'] = [tree for tree in self.population]
+		pop_score['score'] = scores['end']
+		pop_score = pop_score.sort_values(by=['score'], ascending=False)
+		modelTree = pop_score.iloc[0]['tree']
 
+		simudatos = pdr.get_data_yahoo("SAN", start=self.start_date_test, end=self.end_date_test)
+		df_cerebro = bt.feeds.PandasData(dataname = simudatos)
+		indicator.setData(simudatos)
+		cerebro = bt.Cerebro(maxcpus=1)
+		cerebro.optstrategy(TreeStrategy,tree=modelTree)   # Seleccionar estrategia
+		cerebro.addanalyzer(EndStats)						      # Seleccionar analizador
+		cerebro.adddata(df_cerebro)										  # Seleccionar datos
+		cerebro.broker.set_coc(True)
+		cerebro.broker.setcash(10000.0)	# Seleccionar dinero
+		cerebro.broker.setcommission(commission=0.0)
+		ret = cerebro.run()
+		score = pd.DataFrame({r[0].params.tree.ind: r[0].analyzers.endstats.get_analysis() for r in ret}
+		                      ).T.loc[:, ['end', 'growth', 'return']]
 
-
-simudatos = pdr.get_data_yahoo("SAN", start=start_date_train, end=end_date_train)
-# df = yf.download("SAN", start="2017-01-01", end="2017-04-30")  # Otra forma de coger los datos
-df_cerebro = bt.feeds.PandasData(dataname = simudatos)
-indicator.setData(simudatos)
-treeScore = []
-
-
-
-cerebro = bt.Cerebro(maxcpus=1)
-cerebro.optstrategy(TreeStrategy,tree=list(population))   # Seleccionar estrategia
-cerebro.addanalyzer(EndStats)						      # Seleccionar analizador
-cerebro.adddata(df_cerebro)										  # Seleccionar datos
-cerebro.broker.set_coc(True)
-cerebro.broker.setcash(10000.0)	# Seleccionar dinero
-cerebro.broker.setcommission(commission=0.01)
-
-for i in range(numberiter):
-	ts = time.time()
-	ret = cerebro.run()   # EJECUTAR BACKTESTING
-
-	scores = pd.DataFrame({r[0].params.tree.ind: r[0].analyzers.endstats.get_analysis() for r in ret}
-	                      ).T.loc[:, ['end', 'growth', 'return']]
-
-	print(scores)
-	population = NextPopulation(population, scores['end'])
-	# Aquí debería ir la mutación
-	# Por motivos de eficiencia se hace dentro de NextPopulation
-
-	te = time.time()
-	print("El tiempo de simulación es: ",(te - ts))
-
-	indicator.setData(simudatos)
-	cerebro = bt.Cerebro(maxcpus=None)
-	cerebro.optstrategy(TreeStrategy,tree=list(population))   # Seleccionar estrategia
-	cerebro.addanalyzer(EndStats)						      # Seleccionar analizador
-	cerebro.adddata(df_cerebro)										  # Seleccionar datos
-	cerebro.broker.set_coc(True)
-	cerebro.broker.setcash(10000.0)	# Seleccionar dinero
-	cerebro.broker.setcommission(commission=0.01)
-
-	tot = 0
-	for tree in population:
-		tot+=tree.getNumNodes()
-	print(tot/len(population))
+		print('Score on test:')
+		print(score)
 
 
-ret = cerebro.run()
-scores = pd.DataFrame({r[0].params.tree.ind: r[0].analyzers.endstats.get_analysis() for r in ret}
-                      ).T.loc[:, ['end', 'growth', 'return']]
-
-pop_score = pd.DataFrame()
-pop_score['tree'] = [tree for tree in population]
-pop_score['score'] = scores['end']
-pop_score = pop_score.sort_values(by=['score'], ascending=False)
-modelTree = pop_score.iloc[0]['tree']
-
-simudatos = pdr.get_data_yahoo("SAN", start=start_date_test, end=end_date_test)
-df_cerebro = bt.feeds.PandasData(dataname = simudatos)
-indicator.setData(simudatos)
-cerebro = bt.Cerebro(maxcpus=1)
-cerebro.optstrategy(TreeStrategy,tree=modelTree)   # Seleccionar estrategia
-cerebro.addanalyzer(EndStats)						      # Seleccionar analizador
-cerebro.adddata(df_cerebro)										  # Seleccionar datos
-cerebro.broker.set_coc(True)
-cerebro.broker.setcash(10000.0)	# Seleccionar dinero
-cerebro.broker.setcommission(commission=0.0)
-ret = cerebro.run()
-score = pd.DataFrame({r[0].params.tree.ind: r[0].analyzers.endstats.get_analysis() for r in ret}
-                      ).T.loc[:, ['end', 'growth', 'return']]
-
-print('Score on test:')
-print(score)
+sim = Simulate()
+sim.prepare()
+sim.execute()
